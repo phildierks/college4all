46b13ab0836689cb65116d921e742a287b1932c4
Jacques Lybarger-Martel <jacqueslybargermar@students.berkeley.net>
2018-05-18 11:10:46 -0700

    change

--- college4all.js
+++ college4all.js
@@ -1,10 +1,22 @@
+
+
 function showabt(){
     $("#intro").hide();
     $("#about").show();
     $("#stats").hide();
+    $("#homepage").hide();
 }
 function showstats(){
     $("#intro").hide();
     $("#stats").show();
     $("#about").hide();
+    $("#homepage").hide();
+}
+
+function showhome() {
+    $("#homepage").show();
+    $("#intro").hide();
+    $("#about").hide();
+    $("#stats").hide();
+
 }
