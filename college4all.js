42a5b40575e6902fdf20897197162b25de304a6f
phildierks <phillipdierks@students.berkeley.net>
2018-06-07 22:29:18 -0700

    homework

--- college4all.js
+++ college4all.js
@@ -118,25 +118,52 @@ function showand (){
         $("#andrew").hide();
     }
 }
-function getResult(){
-    num = (Math.floor(Math.random() * Math.floor(30)));
-    $.ajax({
-        url: "https://collegereadiness.collegeboard.org/sample-questions/math/calculator-permitted/" + num,
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'html',
-        success: function (result) {
-            myFunction(result);
-        },
-        error: function () {
-            alert('Failed');
-        }
-    });
-}
-function myFunction(result){
-    var html = new DOMParser().parseFromString(result, "text/html");
-    console.log(html);
+
+var questions = [];
+function Question(image,correctAnswer,wrongAnswer1,wrongAnswer2,wrongAnswer3){
+    this.image = image;
+    this.correctAnswer = correctAnswer;
+    this.wrongAnswer1 = wrongAnswer1;
+    this.wrongAnswer2 = wrongAnswer2;
+    this.wrongAnswer3 = wrongAnswer3;
 }
 
+questions.push(new Question("q1","Two","One","Three","Four"));
+questions.push(new Question("q2","7/92","7/90","77/90","84/92"));
+questions.push(new Question("q3","m<19","m>17","m<17","m=17"));
+questions.push(new Question("q4","9","3","-51","144"));
+console.log(questions);
 
+function getQuestion(){
+    num = (Math.floor(Math.random() * Math.floor(4)));
+    console.log(num);
+    var question = questions[num];
+    var display = question.image;
+    var a1 = question.correctAnswer;
+    var a2 = question.wrongAnswer1;
+    var a3 = question.wrongAnswer2;
+    var a4 = question.wrongAnswer3;
+    console.log(display);
+    document.getElementById("collegeB").innerHTML = "<img src='img/"+ display + ".png'>" +
+        "<br><button type=\"button\" id='correct' onclick='correct()' class=\"waves-effect waves-grey btn-flat\"><i ></i>"+a1+"</button>" +"<div id='waiting'</div>"+
+        "<br><br><button type=\"button\"  id='incorrect1' onclick='incorrect1()' class=\"waves-effect waves-grey btn-flat\" class='incorrect'><i ></i>"+a2+"</button>" +
+        "<br><br><button type=\"button\" id='incorrect2' onclick='incorrect2()' class=\"waves-effect waves-grey btn-flat\" class='incorrect'><i ></i>"+a3+"</button>" +
+        "<br><br><button type=\"button\"  id='incorrect3' onclick='incorrect3()' class=\"waves-effect waves-grey btn-flat\" class='incorrect'><i ></i>"+a4+"</button>";
+}
 
+function correct(){
+    document.getElementById('correct').style.background = "green";
+    document.getElementById("waiting").innerHTML= "Great Work! Let's try another one...";
+    setTimeout(function(){getQuestion()},5000);
+}
+
+function incorrect1(){
+    document.getElementById("incorrect1").style.background = 'red';
+}
+
+function incorrect2(){
+    document.getElementById("incorrect2").style.background = 'red';
+}
+function incorrect3(){
+    document.getElementById("incorrect3").style.background = 'red';
+}
